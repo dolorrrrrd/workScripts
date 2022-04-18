2d1b06a4dbcfcb3871acf822f3d2723659e71d9f
dolorrrrrd <72058956+dolorrrrrd@users.noreply.github.com>
2022-04-19 00:21:39 +0800

    Add files via upload

--- Web Scraper/Cotton And Cloud Scraper 2/myScraper.py
+++ Web Scraper/Cotton And Cloud Scraper 2/myScraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 
 def imagedown(url):
     try:
@@ -10,29 +11,42 @@ def imagedown(url):
         for image in images:
             name = image['alt']
             link = image['data-src-zoom-image']
-            with open(name.replace(' ', '-').replace('/', '').replace('"','') + '.jpg', 'wb') as f:
+            with open(name.replace(' ', '-').replace('/', '').replace('"','').replace(':','') + '.jpg', 'wb') as f:
+            # with open(re.sub('\W',name)+ '.jpg', 'wb') as f:
                 im = requests.get(link)
                 f.write(im.content)
                 print('Writing: ', name)
     except:
         pass
 
-postLinks = ["https://www.etsy.com/listing/766993131/barn-quilt-14-sunflower?epik=dj0yJnU9V3c0ZHlpV0tnSHRWT0tRSGRGQkdfdGZ2VXhWSU1Sa04mcD0wJm49aEhNOHpYZmZWeldMS1NfTmpOODdwQSZ0PUFBQUFBRi0yZFdJ&variation0=1293734765",
-"https://www.etsy.com/listing/707119662/sun-flower-barn-quilt-board-2x2?epik=dj0yJnU9WmZ2ckVoWnh1T0hZN2JFMFdJYkpLdHI0dF9MNE1SaTkmcD0wJm49Q3ZoeWtJbERSLXgtY2RhZVVFMXJJZyZ0PUFBQUFBR0RndkJJ",
-"https://www.etsy.com/listing/599933137/patriotic-barn-quilt-svg-liberty-barn?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=patriotic+barn+quilt+design&ref=sr_gallery-1-1&organic_search_click=1",
-"https://www.etsy.com/theTinyEasel/listing/471744745/barn-quilt-hand-painted-2x2?utm_campaign=Share&utm_medium=social_organic&utm_source=DSMT2&utm_term=so.smt&share_time=1524321286000",
-"https://www.etsy.com/listing/814952270/patriotic-barn-quilt-designs?epik=dj0yJnU9Y3p0WktickpVOE9iUndQVWNMT3FHSk9WRXdfbVJIQlkmcD0wJm49U0p2MTdqOGhQckhhWFNHRFY5RVpYdyZ0PUFBQUFBRi0yZ2JZ",
-"https://www.etsy.com/listing/477085041/barn-quilt-1x1-to-hang-on-porchgarden?show_sold_out_detail=1&epik=dj0yJnU9TTNSUTgwbU9iWjBMcXFYWWdRaFlCSThBRUtVX0RJWFcmcD0wJm49MG1UQ1NCczlPdXVPWnNaVWJhM2NmdyZ0PUFBQUFBR0RneDg0",
-"https://www.etsy.com/listing/800280682/glowforge-projects-svg-simple-barn-quilt?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=patriotic+barn+quilt+pattern&ref=sr_gallery-1-4&organic_search_click=1",
-"https://www.etsy.com/listing/806385068/barn-quilt-svg-cut-file-pattern-for?ga_order=most_relevant&ga_search_type=all&ga_view_type=gallery&ga_search_query=modern+barn+quilt+pattern&ref=sr_gallery-1-1&organic_search_click=1",
-"https://www.etsy.com/listing/749186061/sweet-judy-blue-eyes-barn-quilt-4x4?epik=dj0yJnU9aEN4dHdQbkVfdGVoN0NNcldFSFZTM0MyT2U0UHd1aEQmcD0wJm49SkpJZjBWWHRpdEpmNU4wVlVLZ1U3USZ0PUFBQUFBR0Rnemt3"]
+postLinks = ["https://www.etsy.com/listing/818128777/crochet-basket-pattern-sea-urchin-sea?zanpid=10723_1646303411_771aa057eafffbf05ca7ad28506d5914&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1646303411_771aa057eafffbf05ca7ad28506d5914",
+"https://www.etsy.com/listing/863342976/mocha-me-cozy-crochet-pattern-pdf-mug?zanpid=10723_1646309943_59deb3905963a259a54aabddf5920ded&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1646309943_59deb3905963a259a54aabddf5920ded",
+"https://www.etsy.com/listing/252035536/karlas-wrist-warmers-crochet-pattern?zanpid=10723_1650291406_ceab38088e15435eb8129d5c08abe702&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291406_ceab38088e15435eb8129d5c08abe702",
+"https://www.etsy.com/listing/869245339/crochet-hat-pattern-harlequin-puff?zanpid=10723_1650291458_fff1ffd5a05343eab7055d513e864b1a&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291458_fff1ffd5a05343eab7055d513e864b1a",
+"https://www.etsy.com/listing/212541722/pdf-snowflower-lace-coaster-crochet?zanpid=10723_1650291506_1cbf861663ffb3391211d7c43d08d96b&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291506_1cbf861663ffb3391211d7c43d08d96b",
+"https://www.etsy.com/uk/listing/129252681/crochet-cross-bookmark-pattern-tutorial?zanpid=10723_1646309450_9ebd40c7a2092ba41024abc1aed3ad93&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1646309450_9ebd40c7a2092ba41024abc1aed3ad93",
+"https://www.etsy.com/listing/207160622/crochet-pattern-for-the-chicken-or-the?ref=search_srv-4&zanpid=10723_1650291581_edf13849358a1102d47b11426cc0bafb&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291581_edf13849358a1102d47b11426cc0bafb",
+"https://www.etsy.com/listing/841014031/harlequin-crochet-dishcloth-pattern-pdf?zanpid=10723_1650291603_6c933d751ecf888525214954dac5fc5b&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291603_6c933d751ecf888525214954dac5fc5b",
+"https://www.etsy.com/listing/86190638/crochet-pattern-star-by-atergcrochet?zanpid=10723_1650291625_40d96130a1cbb35674632e8e259cae0a&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291625_40d96130a1cbb35674632e8e259cae0a",
+"https://www.etsy.com/uk/listing/865220506/?zanpid=10723_1650291669_01c9f8c84f1958f82eb63273ca820a56&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291669_01c9f8c84f1958f82eb63273ca820a56",
+"https://www.etsy.com/listing/724444291/baby-dribble-bib-pdf-crochet-pattern?zanpid=10723_1650291713_b0f6d987ff5ec405e6acd126a1b9ae6b&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291713_b0f6d987ff5ec405e6acd126a1b9ae6b",
+"https://www.etsy.com/listing/677204069/easy-crochet-pattern-crochet-hat-isabel?zanpid=10723_1650291731_9c5c04e5d201722ef833bb471aa884c9&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291731_9c5c04e5d201722ef833bb471aa884c9",
+"https://www.etsy.com/listing/510416182/crochet-pattern-crochet-christmas-basket?zanpid=10723_1650291748_df717958df01d600acc595527393c8ef&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291748_df717958df01d600acc595527393c8ef",
+"https://www.etsy.com/listing/681242898/crochet-pattern-daybreak-trivet-crochet?zanpid=10723_1650291784_886bd84c8b734979cfaafc1204001b36&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291784_886bd84c8b734979cfaafc1204001b36",
+"https://www.etsy.com/uk/listing/782663544/crochet-pattern-wine-cozy-pattern?zanpid=10723_1650291817_963d505924f6abb35791ac7bdff71534&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291817_963d505924f6abb35791ac7bdff71534",
+"https://www.etsy.com/listing/116631129/crochet-pattern-crochet-christmas?zanpid=10723_1650291838_453ce69f3047ce36d8fef81fb196fc2d&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291838_453ce69f3047ce36d8fef81fb196fc2d",
+"https://www.etsy.com/listing/661355167/crochet-planter-pattern-diy-crochet?zanpid=10723_1650291884_42859ce6a5f8becfc248b01767795aac&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291884_42859ce6a5f8becfc248b01767795aac",
+"https://www.etsy.com/uk/listing/679433629/?g=&zanpid=10723_1650291929_a5e2d34837723681aa4338bc3a300efc&utm_medium=affiliate&utm_source=affiliate_window&utm_campaign=eu_buyer&utm_content=349161&awc=10723_1650291929_a5e2d34837723681aa4338bc3a300efc"]
 
 count = 0
 for links in postLinks:
     count += 1
+    # r = requests.get(links)
+    # soup = BeautifulSoup(r.text, 'html.parser')
+    # images = soup.find('h1').string
     try:
-        os.mkdir(os.path.join("C:\Users\Admin\Desktop\stuff\Cotton And Cloud Pics 2", 'item '+ str(count)))
+        os.mkdir(os.path.join("C:/Users/Admin/Desktop/stuff/Cotton And Cloud Pics 2/35+ Fast and Easy Crochet Gift Ideas Anyone Can Make",'item '+ str(count)))
     except:
         pass
-    os.chdir(os.path.join("C:\Users\Admin\Desktop\stuff\Cotton And Cloud Pics 2", 'item '+str(count)))
+    os.chdir(os.path.join("C:/Users/Admin/Desktop/stuff/Cotton And Cloud Pics 2/35+ Fast and Easy Crochet Gift Ideas Anyone Can Make", 'item '+str(count)))
     imagedown(links)
